555571035389d5a3ee071fc5d0f58690a5e6d702
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-04-20 18:55:55 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,21 @@
-export default function createIteratorObject(report) {
-  return
+function createIteratorObject(report) {
+  let currentDept = 0;
+  let currentEmp = 0;
+  const numDepts = report.departments.length;
+  const numEmps = report.departments[0].employees.length;
+
+  return {
+    next: function() {
+      if (currentEmp === numEmps) {
+        currentDept++;
+        currentEmp = 0;
+      }
+      if (currentDept === numDepts) {
+        return { done: true };
+      }
+      const employee = report.departments[currentDept].employees[currentEmp];
+      currentEmp++;
+      return { value: employee, done: false };
+    }
+  };
 }
